98c73dd6ec5c27978f503adec50fe849c9aa214c
eogiesoba <eogiesoba@gmail.com>
2018-01-30 14:42:51 -0600

    Edited the minimum value function

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function maximum(arr) {//This return the maximum value from an array
 };
 
 function minimum(arr) {//This will return the minimum value from an array
-    var maxVal = arr.reduce(function (x, y) {
+    var minVal = arr.reduce(function (x, y) {
         return Math.min(x, y);
     });
     return minVal;
