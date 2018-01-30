6224ff2a61648b0dc9ef1c7c859ed54437d8bdd8
eogiesoba <eogiesoba@gmail.com>
2018-01-30 14:16:58 -0600

    Added min and max functions

--- main.js
+++ main.js
@@ -7,9 +7,23 @@ var anotherArr = getEvens(newArr);
 
 var thirdArr = double(anotherArr);
 
-var maximum = maximum(thirdArr);
+var maximum = maximum(thirdArr);//This will get the maximum function in the array thirdArr
 
-var minimum = minimum(thirdArr);
+var minimum = minimum(thirdArr);//This will get the minimum function in the array thirdArr
+
+function maximum(arr) {//This return the maximum value from an array
+    var maxVal = arr.reduce(function (x, y) {
+        return Math.max(x, y);
+    });
+    return maxVal;
+};
+
+function minimum(arr) {//This will return the minimum value from an array
+    var maxVal = arr.reduce(function (x, y) {
+        return Math.min(x, y);
+    });
+    return minVal;
+};
 
 console.log(maximum);
 console.log(minimum);
